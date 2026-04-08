23d0aeec4312d1da0762dea47b7477394ebd5929
alfonsocamacho00089-cloud <alfonsocamacho00089@gmail.com>
2026-04-08 15:57:26 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-messaging-compat.js');
 
 // PEGA AQUÍ TUS MISMOS DATOS DE CONFIGURACIÓN
 firebase.initializeApp({
