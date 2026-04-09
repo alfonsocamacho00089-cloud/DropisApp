1a614cddcfa0cd728b57ebc1da6994272959e3a9
alfonsocamacho00089-cloud <alfonsocamacho00089@gmail.com>
2026-04-08 20:52:58 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,51 +1,24 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-
-
-// PEGA AQUÍ TUS MISMOS DATOS DE CONFIGURACIÓN
-
 firebase.initializeApp({
-
   apiKey: "AIzaSyCaUyhX2iBMl4A5xeKeu_4SeE6HClp4V1s",
-
   authDomain: "real-market-elite-2025.firebaseapp.com",
-
   projectId: "real-market-elite-2025",
-
   storageBucket: "real-market-elite-2025.appspot.com",
-
   messagingSenderId: "226489002778",
-
   appId: "1:226489002778:web:6722d21a9e78b33b5b1aa3"
-
 });
-
-
 
 const messaging = firebase.messaging();
 
-
-
-// Este código muestra la notificación cuando llega
-
 messaging.onBackgroundMessage((payload) => {
-
-  const notificationTitle = payload.notification.title || "TuPropina Aviso";
-
+  console.log('Mensaje recibido en segundo plano:', payload);
+  const notificationTitle = payload.notification.title || "TuPropina";
   const notificationOptions = {
-
-    body: payload.notification.body,
-
-    icon: '/logo.png' // Si tienes un logo en tu carpeta, pon el nombre aquí
-
+    body: payload.notification.body || "Nueva actualización de tasa disponible",
+    icon: '/logo.png' 
   };
 
-
-
   self.registration.showNotification(notificationTitle, notificationOptions);
-
 });
-
-  
